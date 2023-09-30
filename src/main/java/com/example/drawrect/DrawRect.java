0e2b8c8bb95f21e077d3c3c2adeab6769067327c
Maxxieisroot <maxxieisroot@pm.me>
2023-09-30 21:27:31 +0200

    Cleaned up code and added comments

--- src/main/java/com/example/drawrect/DrawRect.java
+++ src/main/java/com/example/drawrect/DrawRect.java
@@ -11,43 +11,56 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class DrawRect extends Application {
-    private Button drawRect;
-    private Button drawOval;
     final static Canvas canvas = new Canvas(550,550);
-    private static int step = 10;
-    private static GraphicsContext gc = canvas.getGraphicsContext2D();
+    private final static int step = 10;
+    private final static GraphicsContext gc = canvas.getGraphicsContext2D();
     @Override
     public void start(Stage stage) throws IOException {
+
+    //Draw Rect Button
+        Button drawRect;
         drawRect = new Button();
         drawRect.setText("Draw Rectangle");
         drawRect.setOnAction(evt -> DrawRectangel());
 
+    //Draw oval Button
+        Button drawOval;
         drawOval = new Button();
         drawOval.setText("Draw Oval");
         drawOval.setOnAction(evt -> Drawoval());
         drawOval.setTranslateX(100.0);
+
+    //Setting the Groups and stage
         Group root = new Group();
         Scene s = new Scene(root, 350, 350);
         root.getChildren().addAll(canvas, drawRect, drawOval);
         stage.setTitle("Hello!");
         stage.setScene(s);
         stage.show();
-    }
+    }//End of start
+
+    //Button Function Rectangel
     public static void DrawRectangel(){
 
         gc.clearRect(0,0,canvas.getWidth(), canvas.getHeight());
+
         for (int i = 0; i < 10; i++) {
             gc.strokeRect(10 + i * step,10 + i * step,90 + i * step, 90 + i * step);
-        }
-    }
+        }//End of For loop
+    }//End of DrawRectangel
 
+    //Button Function Oval
     public static void Drawoval() {
+
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
         for (int i = 0; i < 10; i++) {
             gc.strokeOval(10 + i * step, 10 + i * step, 90 + i * step, 90 + i * step);
-        }
-    }
+        }//End of For loop
+    }//End of Drawoval
+
+
     public static void main(String[] args) {
         launch();
-    }
-}
+    }//End of main
+}//End of Class
